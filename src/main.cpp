ee005e091437130e105973c139bba46fccd20115
OE3KUW <wolfgang.kuran@htlstp.ac.at>
2024-02-10 18:26:36 +0100

    mit uart Funktionen erweitert!

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,151 @@
 /***************************************************************************************
-                              a v r.c
-
-                                                                   қuran dez 2023
+                                TimerInterrupt 3xHELS
+                                                                      қuran feb 2024
 **************************************************************************************/
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
-// #include <util/delay.h>
 
-#define TIMER_START_VALUE_0     0x80
-#define COUNT_NR                20000
-#define TRUE                    1
-#define FALSE                   0
+#define TRUE                           1
+#define FALSE                          0
+#define HIGH                           1
+#define LOW                            0
+#define ON_BOARD_LED                   0x20   // PORT B:    ..x. .... 
+#define KEY                            0x04   // PORT D:    .... .x..
+#define TIMER_START_VALUE              0x80
+#define TEN_MSEC                       151
+#define ONE_SEC                        100    // 1 sec = ONE_SEC x 10 msec
+#define BOUD_RATE_9600                 103
 
-#define ON_BOARD_LED            0x20   /*..x. ....*/
+
 
 volatile int flag;
-
-void _wait_64_usec(void);
-void delay(int msec);
+volatile int flagOneSec;
+volatile char recChar;
+void serial_send(char c);
+void writeText(const char * s);
 
 int main(void)
 {
-    //DDRB = (1 << DDB5);
-    DDRB = 0xff;
-    flag = FALSE;
+    // Setup:
+    DDRD = DDRB = 0xff;
+    DDRD &= ~KEY;
+    PORTD |= KEY;
+    flag = flagOneSec = FALSE;
 
+    // Timer Interrupt:
     TIMSK0 = (1 << TOIE0);
-	TCNT0  =  TIMER_START_VALUE_0;
 	TCCR0B = (1 << CS00);
 	TCCR0B = (1 << CS01);
+	TCNT0  =  TIMER_START_VALUE;
+
+    // UART:  
+    UCSR0B = (1 << RXCIE0) | (1 << RXEN0)  | (1 << TXEN0); // frame format: 8 data, 
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // Use 8-bit character sizes
+    UBRR0H = 0;
+    UBRR0L = BOUD_RATE_9600;
+
 
     sei();
-    
-    while(1)
-    {
-//        PORTB = 0xff; 
-//        _delay_ms(500);millis ist nicht vorhanden! 
-//        PORTB = 0;
-//        _delay_ms(500);
 
-        if (flag == TRUE)
+
+    serial_send('s');
+    serial_send('t');
+    serial_send('a');
+    serial_send('r');
+    serial_send('t');
+    serial_send('!');
+ 
+    // endless Loop: 
+    for(;;)
+    {
+        if (flag)
         {
-            flag = FALSE; 
+            flag = FALSE;
             PORTB ^= ON_BOARD_LED; // toggle 
         }
 
+        if (flagOneSec)
+        {
+            flagOneSec = FALSE; 
+            writeText("Elektronik und Technische Informatik ist super!!!"); 
+        }
     }
-    return 0;
+    
+    return 0;  
 }
+
+void serial_send(char c)
+{
+     while ((UCSR0A & (1<<UDRE0))==0);  // wait as long, as the last sending is finished!
+     UDR0 = c;
+}
+
+void writeText(const char * s)
+{
+    const char * p = s; 
+
+    while (*p != '\0') 
+    {
+        serial_send(*p);
+        p++;
+    }
+}
+
+
 
 ISR(TIMER0_OVF_vect)
 {
     static int count = 0; 
-    TCNT0 = TIMER_START_VALUE_0;
+    static int countOneSec = 0;
+    static char lastKey = HIGH, newKey = HIGH;
 
-    count++; 
-
-    if (count > COUNT_NR)
+    TCNT0 = TIMER_START_VALUE;
+    count++;
+    
+    if (count > TEN_MSEC) 
     {
         count = 0;
-        flag = TRUE; 
+        countOneSec++;
+
+        newKey = PIND & KEY;  
+
+        if ((newKey == LOW) && (lastKey != LOW)) // left slope found!
+        {
+            flag = TRUE;
+        }
+
+        lastKey = newKey;
     }
+
+    if (countOneSec >= ONE_SEC)
+    {
+        countOneSec = 0; 
+        flagOneSec = TRUE; 
+    }
+
 }
+
+
+ISR(USART_TX_vect) // Senden abgeschlossen
+{
+    // do nothing
+}
+
+ISR(USART_RX_vect) // Empfang abgeschlossen
+{
+    recChar = UDR0;
+    if (recChar == 'x')   flag = TRUE;
+}
+
+
+/*
+Falls es jemand waten sollte hier ein delay einbauen zu wollen, indem er ganz einfach das Include:
+
+#include <util/delay.h>
+
+einbaut, habe ich zumindest noch diese Funktion auch noch selbst gebaut. 
+
+Hier:
 
 void _wait_64_usec(void)
 {
@@ -77,3 +160,8 @@ int long x;
 
     for (; x > 0 ; x--) _wait_64_usec();
 }
+
+wehe, das verwendet jemand!!! 
+:-)
+
+*/
